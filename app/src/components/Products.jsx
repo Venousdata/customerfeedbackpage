1f82efb3fae07e7555862f5dce52e76edd81f3b9
venousdata <jammyboy122@hotmail.co.uk>
2021-03-29 20:06:46 +0100

    products component to display  product media cards

--- app/src/components/Products.jsx
+++ app/src/components/Products.jsx
@@ -1,23 +1,74 @@
-import { makeStyles, Grid } from "@material-ui/core";
+import {
+  makeStyles,
+  Grid,
+  Card,
+  CardActionArea,
+  CardMedia,
+  CardContent,
+  Typography,
+  CircularProgress,
+} from "@material-ui/core";
 
 const useStyles = makeStyles((theme) => ({
-  products: {
-    // textAlign: "center",
-  },
   gridcontainer: {
-    // textAlign: "center",
+    textAlign: "center",
+  },
+  card: {
+    width: 345,
+    height: 600,
+  },
+  media: {
+    height: 500,
   },
 }));
 
 const Products = ({ products }) => {
   const classes = useStyles();
+  console.log("==========products==========", products);
+
+  const test = (product) => {
+    console.log("TESTING", product);
+  };
 
   return (
-    <Grid container className={classes.gridcontainer}>
-      {products.forEach((product) => (
-        <div>Hello World</div>
-      ))}
-    </Grid>
+    <>
+      {products && products.length ? (
+        <Grid
+          container
+          justify="center"
+          spacing={3}
+          className={classes.gridcontainer}
+        >
+          {products.map((product) => (
+            <Grid key={product.id} item>
+              <Card className={classes.card}>
+                <CardActionArea onClick={() => test(product)}>
+                  <CardMedia
+                    className={classes.media}
+                    image={product.image_src}
+                    title={product.name}
+                  />
+                  <CardContent>
+                    <Typography gutterBottom variant="h5" component="h2">
+                      {product.name}
+                    </Typography>
+                    <Typography
+                      variant="body2"
+                      color="textSecondary"
+                      component="p"
+                    >
+                      {product.description}
+                    </Typography>
+                  </CardContent>
+                </CardActionArea>
+              </Card>
+            </Grid>
+          ))}
+        </Grid>
+      ) : (
+        <CircularProgress />
+      )}
+    </>
   );
 };
 
