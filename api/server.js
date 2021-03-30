e1769bdea69ccf837a6af89802660b075c4e2168
venousdata <jammyboy122@hotmail.co.uk>
2021-03-30 18:42:38 +0100

    uninstall body parser and use built in parser

--- api/server.js
+++ api/server.js
@@ -2,6 +2,8 @@ import express from "express";
 import mountRoutes from "./routes/index.js";
 
 const app = express();
+// parse incoming json payloads
+app.use(express.json());
 mountRoutes(app);
 
 // export app instead of listening to it here to allow for endpoint testing with supertest package
