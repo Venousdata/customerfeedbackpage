45dc961bc2e3dcfde365c823bc0dfd8e354781cf
venousdata <jammyboy122@hotmail.co.uk>
2021-05-06 13:32:44 +0100

    fix broken test

--- app/src/components/__tests__/Product.test.js
+++ app/src/components/__tests__/Product.test.js
@@ -1,10 +1,17 @@
 import React from "react";
 import { render } from "@testing-library/react";
 import Product from "../Product";
+import ReactRouter from "react-router";
+import { BrowserRouter } from "react-router-dom";
 
 describe("Product tests", () => {
   it("Product should render and match snapshot", (done) => {
-    const container = render(<Product />);
+    jest.spyOn(ReactRouter, "useParams").mockReturnValue({ id: "1" });
+    const container = render(
+      <BrowserRouter>
+        <Product />
+      </BrowserRouter>
+    );
     expect(container.firstChild).toMatchSnapshot();
     done();
   });
